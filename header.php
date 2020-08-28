11b0d087c3736f15636487877a946b043ed0a4ef
Nikolay Us <nicholas.us.03@gmail.com>
2020-08-28 23:17:14 +0300

    удалил лишние файлы

--- header.php
+++ header.php
@@ -9,44 +9,7 @@ status_header(200);
     <meta
       charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-<link href=/dist/css/chunk-07c9d3c4.css rel=prefetch>
-<link href=/dist/css/chunk-0cda41aa.css rel=prefetch>
-<link href=/dist/css/chunk-13dee5fe.css rel=prefetch>
-<link href=/dist/css/chunk-15609e94.css rel=prefetch>
-<link href=/dist/css/chunk-1a450389.css rel=prefetch>
-<link href=/dist/css/chunk-2d50840a.css rel=prefetch>
-<link href=/dist/css/chunk-396f5c22.css rel=prefetch>
-<link href=/dist/css/chunk-39e422af.css rel=prefetch>
-<link href=/dist/css/chunk-48a14841.css rel=prefetch>
-<link href=/dist/css/chunk-4bcaa815.css rel=prefetch>
-<link href=/dist/css/chunk-68312676.css rel=prefetch>
-<link href=/dist/css/chunk-eb3dfd8c.css rel=prefetch>
-<link href=/dist/css/chunk-f57197c4.css rel=prefetch>
-<link href=/dist/js/chunk-07c9d3c4.js rel=prefetch>
-<link href=/dist/js/chunk-0bf79c31.js rel=prefetch>
-<link href=/dist/js/chunk-0cda41aa.js rel=prefetch>
-<link href=/dist/js/chunk-13dee5fe.js rel=prefetch>
-<link href=/dist/js/chunk-15609e94.js rel=prefetch>
-<link href=/dist/js/chunk-1a450389.js rel=prefetch>
-<link href=/dist/js/chunk-2d0c8db9.js rel=prefetch>
-<link href=/dist/js/chunk-2d2094f4.js rel=prefetch>
-<link href=/dist/js/chunk-2d21b0fa.js rel=prefetch>
-<link href=/dist/js/chunk-2d21de06.js rel=prefetch>
-<link href=/dist/js/chunk-2d50840a.js rel=prefetch>
-<link href=/dist/js/chunk-396f5c22.js rel=prefetch>
-<link href=/dist/js/chunk-39e422af.js rel=prefetch>
-<link href=/dist/js/chunk-48a14841.js rel=prefetch>
-<link href=/dist/js/chunk-48cbf52c.js rel=prefetch>
-<link href=/dist/js/chunk-4bcaa815.js rel=prefetch>
-<link href=/dist/js/chunk-68312676.js rel=prefetch>
-<link href=/dist/js/chunk-e933e7e2.js rel=prefetch>
-<link href=/dist/js/chunk-eb3dfd8c.js rel=prefetch>
-<link href=/dist/js/chunk-f57197c4.js rel=prefetch>
-<link href=/dist/css/app.css rel=preload as=style>
-<link href=/dist/css/chunk-vendors.css rel=preload as=style>
-<link href=/dist/js/app.js rel=preload as=script>
-<link href=/dist/js/chunk-vendors.js rel=preload as=script>
+
     <?php wp_head(); ?>
   </head>
 
